0a41fa0547bd4c6e886db40370c65a8815114163
jessehartloff <hartloff@buffalo.edu>
2017-02-25 14:34:05 -0500

    initial commit

---
+++ src/DataStructuresChallengePS.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class DataStructuresChallengePS {
+
+
+
+    // 2 points
+    static HashMap<String, Integer> Q1(ArrayList<String> inputList){
+
+        // Return a HashMap containing all the input Strings as keys with values equal to the length of each String
+
+
+
+        return null;
+    }
+
+
+
+    // 3 points
+    static int Q2(ArrayList<ArrayList<Integer>> inputList){
+
+        // Return that sum of all the provided Integers. Note that you are given a ArrayList containing an unknown
+        // number of ArrayLists of Integers (A Matrix of Integers)
+
+
+
+
+        return 0;
+    }
+
+
+
+    // 5 points
+    static ArrayList<Integer> Q3(ArrayList<Integer> input1, ArrayList<Integer> input2){
+
+        // Return an ArrayList containing all the integers that are contained in both the input ArrayLists
+        // (Set/List Intersection)
+
+
+
+        return null;
+    }
+
+
+
+    // 5 points
+    static ArrayList<Integer> Q4(ArrayList<Integer> inputList, HashMap<Integer, Boolean> inputMap){
+
+        // Return an ArrayList containing, for each int in the inputlist:
+        // -The int as-is if the inputMap stores the value true at the key equaling the int
+        // -The negative of the int if the inputMap stores the value false at the key equaling the int
+        // -Do not store the int if it is not contained in the inputMap as a key
+
+
+
+        return null;
+    }
+
+
+
+    public static void main(String[] args) {}
+
+}
